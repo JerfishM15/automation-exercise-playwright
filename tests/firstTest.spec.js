2585abc519ebfab7d6b0a441f441f85c3bcb2c4c
JerfishM15 <jerfish04@gmail.com>
2025-08-11 01:17:28 -0400

    Agregando proyecto para empezar

--- tests/firstTest.spec.js
+++ tests/firstTest.spec.js
@@ -1,7 +1,8 @@
-const { test } = require("@playwright/test");
+const { test, expect } = require("@playwright/test");
 const { BasePage } = require("../pages/BasePage.js");
 
 test("Basic navigation test", async ({ page }) => {
     const basePage = new BasePage(page);
     await basePage.goTo();
+    await expect(page).toHaveTitle('Automation Exercise');
 });
